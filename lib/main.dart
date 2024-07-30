8eccf08f241bb2616e3eeeddaf0c9dadfa84c726
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-07-30 19:07:59 +0300

    Refactor main.dart to improve code readability and remove commented code

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,26 @@
+// import 'package:flutter/material.dart';
+// import 'package:recipe1/screens/home.dart';
+
+// void main() => runApp(const MyApp());
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Recipes',
+//       theme: ThemeData(
+//         primaryColor: const Color(0xff263341),
+//         colorScheme: ColorScheme.fromSwatch().copyWith(secondary: const Color(0xff8DB646)),
+//       ),
+//       home:  HomePage(),
+//     );
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
 import 'package:recipe1/screens/home.dart';
 
@@ -11,11 +34,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Recipes',
-      theme: ThemeData(
-        primaryColor: const Color(0xff263341),
-        colorScheme: ColorScheme.fromSwatch().copyWith(secondary: const Color(0xff8DB646)),
+      theme: _buildThemeData(),
+      home: HomePage(), // Removed 'const' here
+    );
+  }
+
+  // Custom ThemeData to define the app's theme
+  ThemeData _buildThemeData() {
+    return ThemeData(
+      primaryColor: const Color(0xff263341),
+      colorScheme: ColorScheme.fromSwatch().copyWith(
+        secondary: const Color(0xff8DB646),
       ),
-      home:  HomePage(),
+      textTheme: const TextTheme(
+        titleLarge: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.white),
+        bodyLarge: TextStyle(fontSize: 14.0, color: Colors.white),
+      ),
     );
   }
 }
+
